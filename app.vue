59a8041ecc33c2e87da958c13a504110815608b2
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-02-25 23:12:43 +0300

    basic animations added

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup>
 let isHovering = ref(null);
+let loaded = ref(null);
 
 let recentlyPlayedGames = ref(null);
 async function getRecentlyPlayedGames() {
@@ -43,6 +44,11 @@ onBeforeMount(() => {
   getRecentlyPlayedGames();
   getMostPlayedGames();
 });
+onMounted(() => {
+  setTimeout(() => {
+    loaded.value = true;
+  }, 100);
+});
 
 const infoAboutMe =
   "I'm Mehmet, an associate degree 22 years old computer programming student from Turkey. I am currently studying Unity3D, Blender, C#, Vue.js, Nuxt.js, and MySQL. I started my programming journey with front-end web development, by learning HTML, CSS, and JavaScript. After a couple of months, I started learning Vue.js along with the Nuxt.js to advance my front-end skills. But I always wanted to make something related to video games because I love everything about video games and I was always a massive fan of them. So after a year of web development, I find myself in a situation where I had to build something as a school project. While I catch the chance, I started learning Unity to make a video game. And now here I am making games with Unity for fun, using Blender for 3D modeling, and Adobe Photoshop for texturing.";
@@ -51,32 +57,26 @@ const proficienciesFirstColumn = [
   // Tailwind.css colors are used for first and second colors
   // Check https://windicss.org/utilities/general/colors for colors
 
-  // Name | First Color | Second Color | Proficiency Level | Glow Color
-  ["HTML5", "from-orange-700", "to-orange-500", "w-full", "#f97316"],
-  ["CSS3", "from-blue-600", "to-cyan-500", "w-full", "#06b6d4"],
-  ["Tailwind CSS", "from-cyan-700", "to-cyan-500", "w-full", "#06b6d4"],
-  ["JavaScript", "from-yellow-500", "to-yellow-400", "w-4/5", "#fbbf24"],
-  ["Vue.JS", "from-green-600", "to-slate-700", "w-3/5", "#334155"],
-  ["Nuxt.JS", "from-green-500", "to-teal-400", "w-3/5", "#2dd4bf"],
+  // Name | First Color | Second Color | Proficiency Level
+  ["HTML5", "#c2410c", "#f97316", "w-full"],
+  ["CSS3", "#2563eb", "#06b6d4", "w-full"],
+  ["Tailwind CSS", "#0e7490", "#06b6d4", "w-full"],
+  ["JavaScript", "#f59e0b", "#fbbf24", "w-4/5"],
+  ["Vue.JS", "#059669", "#334155", "w-3/5"],
+  ["Nuxt.JS", "#10b981", "#2dd4bf", "w-3/5"],
 ];
 
 const proficienciesSecondColumn = [
   // Tailwind.css colors are used for first and second colors
   // Check https://windicss.org/utilities/general/colors for colors
 
-  // Name | First Color | Second Color | Proficiency Level | Glow Color
-  ["C#", "from-purple-600", "to-fuchsia-500", "w-3/5", "#d946ef"],
-  ["MySQL", "from-blue-600", "to-slate-600", "w-4/5", "#475569"],
-  ["Unity3D", "from-black", "to-gray-700", "w-5/10", "#374151"],
-  ["Blender", "from-orange-600", "to-orange-400", "w-1/5", "#fb923c"],
-  ["Adobe Photoshop", "from-slate-800", "to-sky-500", "w-5/10", "#0ea5e9"],
-  [
-    "Adobe After Effects",
-    "from-blue-800",
-    "to-indigo-500",
-    "w-5/10",
-    "#6366f1",
-  ],
+  // Name | First Color | Second Color | Proficiency Level
+  ["C#", "#7c3aed", "#d946ef", "w-3/5"],
+  ["MySQL", "#2563eb", "#475569", "w-4/5"],
+  ["Unity3D", "#000", "#374151", "w-5/10"],
+  ["Blender", "#ea580c", "#fb923c", "w-1/5"],
+  ["Adobe Photoshop", "#1E293B", "#0ea5e9", "w-5/10"],
+  ["Adobe After Effects", "#1e40af", "#6366f1", "w-5/10"],
 ];
 
 function calculateLastTimePlayed(seconds) {
@@ -132,39 +132,78 @@ let showDev = ref(true);
     <div class="color-wrapper absolute h-full min-w-full min-h-full -z-10" />
     <div class="mx-auto px-40 py-40 text-slate-200/95">
       <section>
-        <h1 class="text-5xl font-bold mb-6">Hello</h1>
-        <p class="text-3xl font-normal text-slate-400">
+        <h1
+          class="text-5xl font-bold mb-6 transform transition-all duration-1100"
+          :class="[
+            loaded == true
+              ? 'translate-x-0 visible opacity-100'
+              : '-translate-x-72 invisible opacity-0',
+          ]"
+        >
+          Hello
+        </h1>
+        <p
+          class="text-3xl font-normal text-slate-400 transform transition-all duration-1000"
+          :class="[
+            loaded == true
+              ? 'translate-x-0 visible opacity-100'
+              : '-translate-x-48 invisible opacity-0',
+          ]"
+        >
           {{ infoAboutMe }}
         </p>
       </section>
       <section>
-        <h1 class="text-5xl font-bold mb-6 mt-10">Proficiencies</h1>
+        <h1
+          class="text-5xl font-bold mb-6 mt-10 transform transition-all duration-1300"
+          :class="[
+            loaded == true
+              ? 'translate-x-0 visible opacity-100'
+              : 'translate-x-48 invisible opacity-0',
+          ]"
+        >
+          Proficiencies
+        </h1>
         <div class="flex w-full">
           <div class="w-1/2 mr-4">
-            <div v-for="item in proficienciesFirstColumn">
+            <div
+              v-for="item in proficienciesFirstColumn"
+              class="transform transition-all"
+              :class="[
+                loaded == true
+                  ? `translate-x-0 visible opacity-100 duration-${1000}`
+                  : `-translate-x-48 invisible opacity-0 `,
+              ]"
+            >
               <h2 class="font-semibold text-2xl">{{ item[0] }}</h2>
               <div class="bg-slate-900 rounded-md">
                 <div
-                  :class="[
-                    `bg-gradient-to-r ${item[1]} ${item[2]} ${item[3]} py-5 rounded-md my-3`,
-                  ]"
+                  :class="[`${item[3]} py-5 rounded-md my-3`]"
                   :style="[
-                    `box-shadow: 20px 0 60px ${item[4]}, -20px 0 60px #000;`,
+                    `box-shadow: 20px 0 60px ${item[2]}, -20px 0 60px #000;
+                    background:linear-gradient(to right,${item[1]},${item[2]})`,
                   ]"
                 />
               </div>
             </div>
           </div>
           <div class="w-1/2 ml-4">
-            <div v-for="item in proficienciesSecondColumn">
+            <div
+              v-for="item in proficienciesSecondColumn"
+              class="transform transition-all"
+              :class="[
+                loaded == true
+                  ? `translate-x-0 visible opacity-100 duration-${1000}`
+                  : `translate-x-48 invisible opacity-0 `,
+              ]"
+            >
               <h2 class="font-semibold text-2xl">{{ item[0] }}</h2>
               <div class="bg-slate-900 rounded-md">
                 <div
-                  :class="[
-                    `bg-gradient-to-r ${item[1]} ${item[2]} ${item[3]} py-5 rounded-md my-3`,
-                  ]"
+                  :class="[`${item[3]} py-5 rounded-md my-3`]"
                   :style="[
-                    `box-shadow: 20px 0 60px ${item[4]}, -20px 0 60px #000;`,
+                    `box-shadow: 20px 0 60px ${item[2]}, -20px 0 60px #000;
+                    background:linear-gradient(to right,${item[1]},${item[2]})`,
                   ]"
                 />
               </div>
@@ -183,20 +222,27 @@ let showDev = ref(true);
       </section>
       <section>
         <h1 class="text-5xl font-bold mb-6 mt-10">Recently Played Games</h1>
-        <div v-if="recentlyPlayedGames != null">
+        <div
+          class="flex flex-wrap gap-2 justify-between"
+          v-if="recentlyPlayedGames != null"
+        >
           <div
-            class="inline-flex w-1/3 transition-all duration-300 p-2"
+            class="w-[calc(33.3%-0.5rem)] transition-all duration-300 border-2"
+            :class="[
+              isHovering == recentlyPlayedGames.indexOf(item)
+                ? 'border-slate-400'
+                : 'border-transparent',
+            ]"
             v-for="item in recentlyPlayedGames"
           >
             <div class="relative inline-flex overflow-hidden w-full">
               <img
                 class="transform transition-all duration-300 w-full"
-                :class="{
-                  'scale-110 filter blur-[1px]':
-                    isHovering == recentlyPlayedGames.indexOf(item),
-                  'scale-100 filter blur-[0px]':
-                    isHovering != recentlyPlayedGames.indexOf(item),
-                }"
+                :class="[
+                  isHovering == recentlyPlayedGames.indexOf(item)
+                    ? 'scale-110 filter blur-[1px]'
+                    : 'scale-100 filter blur-[0px]',
+                ]"
                 :src="`https://steamcdn-a.akamaihd.net/steam/apps/${item.appid}/header.jpg`"
                 :alt="item.name + ' Banner'"
               />
@@ -226,22 +272,29 @@ let showDev = ref(true);
       </section>
       <section>
         <h1 class="text-5xl font-bold mb-6 mt-10">Most Played Games</h1>
-        <div v-if="mostPlayedGames != null">
+        <div
+          class="flex flex-wrap gap-2 justify-between"
+          v-if="mostPlayedGames != null"
+        >
           <div
-            class="inline-flex w-1/3 transition-all duration-300 p-2"
+            class="w-[calc(33.3%-0.5rem)] transition-all duration-300 border-2"
+            :class="[
+              isHovering == mostPlayedGames.indexOf(item) + 6
+                ? 'border-slate-400'
+                : 'border-transparent',
+            ]"
             v-for="item in mostPlayedGames"
           >
             <div class="relative inline-flex overflow-hidden w-full">
               <img
                 class="transform transition-all duration-300 w-full"
-                :class="{
-                  'scale-110 filter blur-[1px]':
-                    isHovering == mostPlayedGames.indexOf(item) + 6,
-                  'scale-100 filter blur-[0px]':
-                    isHovering != mostPlayedGames.indexOf(item) + 6,
-                }"
+                :class="[
+                  isHovering == mostPlayedGames.indexOf(item) + 6
+                    ? 'scale-110 filter blur-[1px]'
+                    : 'scale-100 filter blur-[0px]',
+                ]"
                 :src="`https://steamcdn-a.akamaihd.net/steam/apps/${item.appid}/header.jpg`"
-                :alt="item.name + ' Icon'"
+                :alt="item.name + ' Banner'"
               />
               <a
                 @mouseenter="isHovering = mostPlayedGames.indexOf(item) + 6"
@@ -284,23 +337,22 @@ let showDev = ref(true);
   background-image: radial-gradient(
     ellipse at left top,
     transparent -100%,
-    rgba(10, 10, 10) 40%
+    rgb(10, 10, 10) 40%
   );
 }
 
 .change-colors {
   background: linear-gradient(
-    -45deg,
+    90deg,
     #ff3c00,
-    #ff0062,
     #e92c2c,
+    #ff0062,
     #cc00ff,
-    #3529db,
     #0051ff,
     #00bbff,
     #00ffc3
   );
-  animation: color-change 10s ease infinite;
+  animation: color-change 20s ease infinite;
   background-size: 400% 400%;
 }
 
@@ -309,7 +361,7 @@ let showDev = ref(true);
     background-position: 0% 50%;
   }
   50% {
-    background-position: 100% 50%;
+    background-position: 115% 50%;
   }
   100% {
     background-position: 0% 50%;
--- windi.config.ts
+++ windi.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
-
+safelist:[
+    "-translate-x-96","-translate-x-72","-translate-x-48","translate-x-0","translate-x-48","invisible","visible"
+]
 })
