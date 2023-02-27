aae387b5bc0b062940b0c62bf028a698828ec657
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-02-28 02:44:16 +0300

    more animations added

--- app.vue
+++ app.vue
@@ -9,6 +9,11 @@ var recentlyPlayedGames = reactive(await recentGames.getRecentlyPlayedGames());
 var mostPlayedGames = reactive(await topGames.getMostPlayedGames());
 
 var isHovering = ref(null);
+var essentialsLoaded = ref(false);
+var proficienciesAnim = ref(false);
+onMounted(() => {
+  essentialsLoaded.value = true;
+});
 
 var animationDelay = 0;
 function increaseDelay() {
@@ -23,11 +28,11 @@ function animDirection(itemIndex) {
   else return "fade-left";
 }
 
-const infoAboutMe = `I'm Mehmet, an associate degree ${(
+const infoAboutMe = `I'm Mehmet, a ${(
   new Date().getUTCFullYear() - 2000.8
 ).toFixed(
   0
-)} years old computer programming student from Turkey. I am currently studying Unity3D, Blender, C#, Vue.js, Nuxt.js, and MySQL. I started my programming journey with front-end web development, by learning HTML, CSS, and JavaScript. After a couple of months, I started learning Vue.js along with the Nuxt.js to advance my front-end skills. But I always wanted to make something related to video games because I love everything about video games and I was always a massive fan of them. So after a year of web development, I find myself in a situation where I had to build something as a school project. While I catch the chance, I started learning Unity to make a video game. And now here I am making games with Unity for fun, using Blender for 3D modeling, and Adobe Photoshop for texturing.`;
+)} years old associate degree computer programming student. I enjoy programming and 3D modeling. I am currently studying Blender, C#, Vue.js, and Nuxt.js. Also, I love video games, I try to code games in Unity3D for fun in my free time.`;
 
 const currentlyWorkingOn = "A news website to post about video games,";
 
@@ -39,9 +44,9 @@ const proficienciesFirstColumn = [
   ["HTML5", "#c2410c", "#f97316", "w-full"],
   ["CSS3", "#2563eb", "#06b6d4", "w-full"],
   ["Tailwind CSS", "#0e7490", "#06b6d4", "w-full"],
-  ["JavaScript", "#f59e0b", "#fbbf24", "w-4/5"],
-  ["Vue.JS", "#059669", "#334155", "w-3/5"],
-  ["Nuxt.JS", "#10b981", "#2dd4bf", "w-3/5"],
+  ["JavaScript", "#f59e0b", "#fbbf24", "w-7/10"],
+  ["Vue.JS", "#059669", "#334155", "w-6/10"],
+  ["Nuxt.JS", "#10b981", "#2dd4bf", "w-6/10"],
 ];
 
 const proficienciesSecondColumn = [
@@ -49,36 +54,73 @@ const proficienciesSecondColumn = [
   // Check https://windicss.org/utilities/general/colors for colors
 
   // Name | First Color | Second Color | Proficiency Level
-  ["C#", "#7c3aed", "#d946ef", "w-3/5"],
-  ["MySQL", "#2563eb", "#475569", "w-4/5"],
-  ["Unity3D", "#000", "#374151", "w-5/10"],
-  ["Blender", "#ea580c", "#fb923c", "w-1/5"],
-  ["Adobe Photoshop", "#1E293B", "#0ea5e9", "w-5/10"],
-  ["Adobe After Effects", "#1e40af", "#6366f1", "w-5/10"],
+  ["C#", "#7c3aed", "#d946ef", "w-1/2"],
+  ["MySQL", "#2563eb", "#475569", "w-7/10"],
+  ["Unity3D", "#000", "#374151", "w-2/5"],
+  ["Blender", "#ea580c", "#fb923c", "w-1/10"],
+  ["Adobe Photoshop", "#1E293B", "#0ea5e9", "w-1/2"],
+  ["Adobe After Effects", "#1e40af", "#6366f1", "w-1/2"],
 ];
 </script>
 <template>
-  <div class="min-h-screen min-w-full h-full relative">
-    <div class="change-colors absolute h-full min-w-full min-h-full -z-20" />
-    <div class="color-wrapper absolute h-full min-w-full min-h-full -z-10" />
-    <div class="mx-auto px-40 py-40 text-slate-200/95">
-      <section>
-        <h1 class="text-5xl font-bold mb-6">Hello</h1>
-        <p class="text-3xl font-normal text-slate-400">
+  <div
+    class="min-h-screen min-w-full h-full relative bg-[rgb(10,10,10)] text-slate-200/95"
+  >
+    <div class="change-colors absolute min-w-full min-h-screen z-0" />
+    <div class="color-wrapper absolute min-w-full min-h-screen z-10" />
+    <div class="mx-auto px-60">
+      <section
+        class="min-h-screen flex items-center flex-col text-center justify-center relative z-20 pt-[12.5%]"
+      >
+        <h1
+          class="text-5xl font-bold mb-6 transform transition-all duration-800"
+          :class="[
+            essentialsLoaded == true
+              ? 'translate-y-0 opacity-100'
+              : 'translate-y-40 opacity-0',
+          ]"
+        >
+          Hello
+        </h1>
+        <p
+          class="text-3xl font-normal text-slate-400 transform transition-all duration-800"
+          :class="[
+            essentialsLoaded == true
+              ? 'translate-y-0 opacity-100'
+              : 'translate-y-40 opacity-0',
+          ]"
+        >
           {{ infoAboutMe }}
         </p>
+        <hr
+          class="absolute top-0 mt-[12.5%] border-1 border-slate-500 relative z-20 transition-all duration-1000"
+          :class="{
+            'w-0': essentialsLoaded == false,
+            'w-1/5': essentialsLoaded == true,
+          }"
+        />
       </section>
       <section>
-        <h1 class="text-5xl font-bold mb-6 mt-10">Proficiencies</h1>
+        <h1
+          class="text-5xl font-bold mb-6"
+          data-aos="fade-left"
+          data-aos-duration="900"
+        >
+          Proficiencies
+        </h1>
         <div class="flex w-full">
           <div class="w-1/2 mr-4">
-            <div v-for="item in proficienciesFirstColumn">
+            <div
+              id="dat"
+              v-for="item in proficienciesFirstColumn"
+              data-aos="fade-right"
+              :data-aos-delay="`${increaseDelay()}`"
+            >
               <h2 class="font-semibold text-2xl">{{ item[0] }}</h2>
               <div class="bg-slate-900 rounded-md">
                 <div
-                  :class="[
-                    `${item[3]} py-5 rounded-md my-3 transition-all duration-1000`,
-                  ]"
+                  class="py-5 rounded-md my-3 transition-all duration-1000"
+                  :class="[essentialsLoaded == true ? `${item[3]}` : 'w-0']"
                   :style="[
                     `box-shadow: 20px 0 60px ${item[2]}, -20px 0 60px #000;
                     background:linear-gradient(to right,${item[1]},${item[2]})`,
@@ -88,13 +130,16 @@ const proficienciesSecondColumn = [
             </div>
           </div>
           <div class="w-1/2 ml-4">
-            <div v-for="item in proficienciesSecondColumn">
+            <div
+              v-for="item in proficienciesSecondColumn"
+              data-aos="fade-right"
+              :data-aos-delay="`${increaseDelay()}`"
+            >
               <h2 class="font-semibold text-2xl">{{ item[0] }}</h2>
               <div class="bg-slate-900 rounded-md">
                 <div
-                  :class="[
-                    `${item[3]} py-5 rounded-md my-3 transition-all duration-1000`,
-                  ]"
+                  class="py-5 rounded-md my-3 transition-all duration-1000"
+                  :class="[essentialsLoaded == true ? `${item[3]}` : 'w-0']"
                   :style="[
                     `box-shadow: 20px 0 60px ${item[2]}, -20px 0 60px #000;
                     background:linear-gradient(to right,${item[1]},${item[2]})`,
@@ -107,16 +152,16 @@ const proficienciesSecondColumn = [
       </section>
       <section>
         <h1
-          class="text-5xl font-bold mb-4 mt-10"
+          class="text-5xl font-bold mb-4 mt-25"
           data-aos="fade-right"
-          data-aos-duration="600"
+          data-aos-duration="900"
         >
           Currently Working On
         </h1>
         <p
           class="text-3xl font-normal text-slate-400"
-          data-aos="fade-left"
-          data-aos-duration="800"
+          data-aos="fade-right"
+          data-aos-duration="550"
         >
           {{ currentlyWorkingOn }}
           <a href="" target="_blank" class="text-cyan-500 font-medium">
@@ -126,8 +171,8 @@ const proficienciesSecondColumn = [
       </section>
       <section>
         <h1
-          class="text-5xl font-bold mb-6 mt-10"
-          data-aos="fade-right"
+          class="text-5xl font-bold mb-6 mt-25"
+          data-aos="fade-left"
           data-aos-duration="1100"
         >
           Recently Played Games
@@ -179,7 +224,7 @@ const proficienciesSecondColumn = [
           </div>
         </div>
       </section>
-      <section>
+      <section class="pb-[10%]">
         <h1
           class="text-5xl font-bold mb-6 mt-10"
           data-aos="fade-left"
@@ -233,6 +278,9 @@ const proficienciesSecondColumn = [
         </div>
       </section>
     </div>
+    <footer
+      class="bg-[rgb(3,3,3)] py-5 text-sm text-center text-slate-400"
+    ></footer>
   </div>
 </template>
 <style>
@@ -250,18 +298,18 @@ const proficienciesSecondColumn = [
 }
 
 ::-webkit-scrollbar-thumb {
-  background: rgb(60, 60, 75);
+  background: rgb(40, 40, 45);
 }
 
 ::-webkit-scrollbar-thumb:hover {
-  background: rgb(80, 80, 100);
+  background: rgb(60, 60, 68);
 }
 
 .color-wrapper {
   background-image: radial-gradient(
     ellipse at left top,
     transparent -100%,
-    rgb(10, 10, 10) 40%
+    rgb(10, 10, 10) 35%
   );
 }
 
@@ -276,11 +324,11 @@ const proficienciesSecondColumn = [
     #00bbff,
     #00ffc3
   );
-  animation: color-change 20s ease infinite;
+  animation: change-colors 10s ease infinite;
   background-size: 400% 400%;
 }
 
-@keyframes color-change {
+@keyframes change-colors {
   0% {
     background-position: 0% 50%;
   }
