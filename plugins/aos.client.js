61392b0645277fb726bbb9e280be4bab389cb2cb
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-03-01 19:09:23 +0300

    config file organizing

--- plugins/aos.client.js
+++ plugins/aos.client.js
@@ -3,17 +3,9 @@ import "aos/dist/aos.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.AOS = AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: "aos-init", // class applied after initialization
-    animatedClassName: "aos-animate", // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    // Check for detailed usage of the plugin: https://github.com/michalsnik/aos
+    // See how it works https://michalsnik.github.io/aos/
 
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 0, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 500, // values from 0 to 3000, with step 50ms
