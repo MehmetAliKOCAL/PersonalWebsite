682e9967a367fa8a0bb2443d8d0081faab0d781a
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-03-01 19:08:59 +0300

    vueuse lib added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-windicss", "@pinia/nuxt",],
+  modules: ["nuxt-windicss", "@pinia/nuxt", '@vueuse/nuxt',],
   plugins: [
     { src: '~/plugins/aos.client', mode: 'client' },
   ],
---
+++ plugins/vue-gtag.client.js
@@ -0,0 +1,12 @@
+import VueGtag from "vue-gtag";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      // Put your Google Analytics Measurement ID below if you want to use Google Analytics.
+      // For more info: https://analytics.google.com/
+
+      id: "G-5T2GDLFX5S",
+    },
+  });
+});
