a3ec730ea6dd7297a57542b024371f314bee3650
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-03-01 19:09:47 +0300

    google analytics and og meta tags added

--- app.vue
+++ app.vue
@@ -3,6 +3,11 @@ import {
   useRecentGamesStore,
   useMostPlayedGamesStore,
 } from "~~/store/steamInfo";
+import { useElementVisibility } from "@vueuse/core";
+
+const target = ref(null);
+const targetIsVisible = useElementVisibility(target);
+
 const recentGames = useRecentGamesStore();
 const topGames = useMostPlayedGamesStore();
 var recentlyPlayedGames = reactive(await recentGames.getRecentlyPlayedGames());
@@ -36,7 +41,7 @@ const infoAboutMe = `I'm Mehmet, a ${(
 
 const currentlyWorkingOn = "A news website to post about video games,";
 
-const proficienciesFirstColumn = [
+const proficiencies = [
   // Tailwind.css colors are used
   // Check https://windicss.org/utilities/general/colors for colors
 
@@ -47,13 +52,6 @@ const proficienciesFirstColumn = [
   ["JavaScript", "#f59e0b", "#fbbf24", "w-7/10"],
   ["Vue.JS", "#059669", "#334155", "w-6/10"],
   ["Nuxt.JS", "#10b981", "#2dd4bf", "w-6/10"],
-];
-
-const proficienciesSecondColumn = [
-  // Tailwind.css colors are used
-  // Check https://windicss.org/utilities/general/colors for colors
-
-  // Name | First Color | Second Color | Proficiency Level
   ["C#", "#7c3aed", "#d946ef", "w-1/2"],
   ["MySQL", "#2563eb", "#475569", "w-7/10"],
   ["Unity3D", "#000", "#374151", "w-2/5"],
@@ -63,64 +61,98 @@ const proficienciesSecondColumn = [
 ];
 </script>
 <template>
-  <div
-    class="min-h-screen min-w-full h-full relative bg-[rgb(10,10,10)] text-slate-200/95"
-  >
-    <div class="change-colors absolute min-w-full min-h-screen z-0" />
-    <div class="color-wrapper absolute min-w-full min-h-screen z-10" />
-    <div class="mx-auto px-60">
-      <section
-        class="min-h-screen flex items-center flex-col text-center justify-center relative z-20 pt-[12.5%]"
-      >
-        <h1
-          class="text-5xl font-bold mb-6 transform transition-all duration-800"
-          :class="[
-            essentialsLoaded == true
-              ? 'translate-y-0 opacity-100'
-              : 'translate-y-40 opacity-0',
-          ]"
+  <div>
+    <!-- OpenGraph meta tags, edit it for yourself. -->
+    <Head>
+      <Title>Mehmet Ali KOÇAL</Title>
+      <Meta
+        name="description"
+        content="Mehmet is a front-end web developer, student, and video game lover. He develops websites or tries to make games in his spare time. Click to learn more!"
+      />
+      <Meta property="og:url" content="https://gwyndev.netlify.app" />
+      <Meta property="og:type" content="website" />
+      <Meta property="og:title" content="Mehmet Ali KOÇAL" />
+      <Meta
+        property="og:description"
+        content="Mehmet is a front-end web developer, student, and video game lover. He develops websites or tries to make games in his spare time. Click to learn more!"
+      />
+      <Meta
+        property="og:image"
+        content="https://avatars.githubusercontent.com/u/102923401?v=4"
+      />
+      <Meta name="twitter:card" content="summary_large_image" />
+      <Meta property="twitter:domain" content="gwyndev.netlify.app" />
+      <meta property="twitter:url" content="https://gwyndev.netlify.app" />
+      <Meta name="twitter:title" content="Mehmet Ali KOÇAL" />
+      <Meta
+        name="twitter:description"
+        content="Mehmet is a front-end web developer, student, and video game lover. He develops websites or tries to make games in his spare time. Click to learn more!"
+      />
+      <Meta
+        name="twitter:image"
+        content="https://avatars.githubusercontent.com/u/102923401?v=4"
+      />
+    </Head>
+
+    <div
+      class="min-h-screen min-w-full h-full relative bg-[rgb(10,10,10)] text-slate-200/95"
+    >
+      <div class="change-colors absolute min-w-full min-h-screen z-0" />
+      <div class="color-wrapper absolute min-w-full min-h-screen z-10" />
+      <div class="mx-auto px-60">
+        <section
+          class="min-h-screen flex items-center flex-col text-center justify-center relative z-20 pt-[12.5%]"
         >
-          Hello
-        </h1>
-        <p
-          class="text-3xl font-normal text-slate-400 transform transition-all duration-800"
-          :class="[
-            essentialsLoaded == true
-              ? 'translate-y-0 opacity-100'
-              : 'translate-y-40 opacity-0',
-          ]"
-        >
-          {{ infoAboutMe }}
-        </p>
-        <hr
-          class="absolute top-0 mt-[12.5%] border-1 border-slate-500 relative z-20 transition-all duration-1000"
-          :class="{
-            'w-0': essentialsLoaded == false,
-            'w-1/5': essentialsLoaded == true,
-          }"
-        />
-      </section>
-      <section>
-        <h1
-          class="text-5xl font-bold mb-6"
-          data-aos="fade-left"
-          data-aos-duration="900"
-        >
-          Proficiencies
-        </h1>
-        <div class="flex w-full">
-          <div class="w-1/2 mr-4">
+          <h1
+            class="text-5xl font-bold mb-6 transform transition-all duration-800"
+            :class="[
+              essentialsLoaded
+                ? 'translate-y-0 opacity-100'
+                : 'translate-y-22 opacity-0',
+            ]"
+          >
+            Hello
+          </h1>
+          <p
+            class="text-3xl font-normal text-slate-400 transform transition-all duration-800"
+            :class="[
+              essentialsLoaded
+                ? 'translate-y-0 opacity-100'
+                : 'translate-y-22 opacity-0',
+            ]"
+          >
+            {{ infoAboutMe }}
+          </p>
+          <hr
+            class="absolute top-0 mt-[12.5%] border-1 border-slate-500 relative z-20 transition-all duration-1000 transform"
+            :class="[
+              essentialsLoaded ? 'w-1/5 translate-y-0' : 'w-0 -translate-y-22',
+            ]"
+          />
+        </section>
+        <section>
+          <h1
+            class="text-5xl font-bold mb-6"
+            data-aos="fade-left"
+            data-aos-duration="900"
+          >
+            Proficiencies
+          </h1>
+          <div
+            ref="target"
+            class="flex w-full flex-wrap justify-between gap-x-4"
+          >
             <div
-              id="dat"
-              v-for="item in proficienciesFirstColumn"
+              v-for="item in proficiencies"
               data-aos="fade-right"
               :data-aos-delay="`${increaseDelay()}`"
+              class="w-[calc(50%-1rem)]"
             >
               <h2 class="font-semibold text-2xl">{{ item[0] }}</h2>
               <div class="bg-slate-900 rounded-md">
                 <div
-                  class="py-5 rounded-md my-3 transition-all duration-1000"
-                  :class="[essentialsLoaded == true ? `${item[3]}` : 'w-0']"
+                  class="py-5 rounded-md my-3 transition-all duration-2000"
+                  :class="[targetIsVisible ? `${item[3]}` : 'w-0']"
                   :style="[
                     `box-shadow: 20px 0 60px ${item[2]}, -20px 0 60px #000;
                     background:linear-gradient(to right,${item[1]},${item[2]})`,
@@ -129,158 +161,145 @@ const proficienciesSecondColumn = [
               </div>
             </div>
           </div>
-          <div class="w-1/2 ml-4">
+        </section>
+        <section>
+          <h1
+            class="text-5xl font-bold mb-4 mt-25"
+            data-aos="fade-right"
+            data-aos-duration="900"
+          >
+            Currently Working On
+          </h1>
+          <p
+            class="text-3xl font-normal text-slate-400"
+            data-aos="fade-right"
+            data-aos-duration="550"
+          >
+            {{ currentlyWorkingOn }}
+            <a href="" target="_blank" class="text-cyan-500 font-medium">
+              check it!
+            </a>
+          </p>
+        </section>
+        <section>
+          <h1
+            class="text-5xl font-bold mb-6 mt-20"
+            data-aos="fade-left"
+            data-aos-duration="1100"
+          >
+            Recently Played Games
+          </h1>
+          <div
+            class="flex flex-wrap gap-2 justify-between"
+            v-if="recentlyPlayedGames != null"
+          >
             <div
-              v-for="item in proficienciesSecondColumn"
-              data-aos="fade-right"
+              class="w-[calc(33.3%-0.5rem)] transition-all duration-300 border-1 border-transparent hover:border-slate-400"
+              v-for="item in recentlyPlayedGames"
+              :data-aos="`${animDirection(recentlyPlayedGames.indexOf(item))}`"
               :data-aos-delay="`${increaseDelay()}`"
             >
-              <h2 class="font-semibold text-2xl">{{ item[0] }}</h2>
-              <div class="bg-slate-900 rounded-md">
-                <div
-                  class="py-5 rounded-md my-3 transition-all duration-1000"
-                  :class="[essentialsLoaded == true ? `${item[3]}` : 'w-0']"
-                  :style="[
-                    `box-shadow: 20px 0 60px ${item[2]}, -20px 0 60px #000;
-                    background:linear-gradient(to right,${item[1]},${item[2]})`,
-                  ]"
+              <div class="relative inline-flex overflow-hidden w-full">
+                <img
+                  class="transform transition-all duration-300 w-full"
+                  :class="{
+                    'scale-110 filter blur-[1px]':
+                      isHovering == recentlyPlayedGames.indexOf(item),
+                  }"
+                  :src="`https://steamcdn-a.akamaihd.net/steam/apps/${item.appid}/header.jpg`"
+                  :alt="item.name"
                 />
+                <a
+                  @mouseenter="isHovering = recentlyPlayedGames.indexOf(item)"
+                  @mouseleave="isHovering = null"
+                  class="absolute w-full h-full flex flex-col justify-center items-center bg-black/70 text-white hover:bg-black/80 active:bg-black/85 transition-all duration-300 cursor-pointer"
+                  :href="`https://store.steampowered.com/app/${item.appid}/`"
+                  target="_blank"
+                >
+                  <h2 class="text-2xl font-bold text-center">
+                    {{ item.name }}
+                  </h2>
+                  <p class="text-lg font-semibold mt-2 -mb-2">
+                    {{
+                      recentGames.calculatePlayTime(
+                        item.playtime_2weeks,
+                        false
+                      ) + " in last 2 weeks"
+                    }}
+                  </p>
+                  <p class="text-lg font-semibold">
+                    {{
+                      recentGames.calculatePlayTime(
+                        item.playtime_forever,
+                        false
+                      ) + " total"
+                    }}
+                  </p>
+                </a>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section>
-        <h1
-          class="text-5xl font-bold mb-4 mt-25"
-          data-aos="fade-right"
-          data-aos-duration="900"
-        >
-          Currently Working On
-        </h1>
-        <p
-          class="text-3xl font-normal text-slate-400"
-          data-aos="fade-right"
-          data-aos-duration="550"
-        >
-          {{ currentlyWorkingOn }}
-          <a href="" target="_blank" class="text-cyan-500 font-medium">
-            check it!
-          </a>
-        </p>
-      </section>
-      <section>
-        <h1
-          class="text-5xl font-bold mb-6 mt-25"
-          data-aos="fade-left"
-          data-aos-duration="1100"
-        >
-          Recently Played Games
-        </h1>
-        <div
-          class="flex flex-wrap gap-2 justify-between"
-          v-if="recentlyPlayedGames != null"
-        >
-          <div
-            class="w-[calc(33.3%-0.5rem)] transition-all duration-300 border-1 border-transparent hover:border-slate-400"
-            v-for="item in recentlyPlayedGames"
-            :data-aos="`${animDirection(recentlyPlayedGames.indexOf(item))}`"
-            :data-aos-delay="`${increaseDelay()}`"
+        </section>
+        <section class="pb-[10%]">
+          <h1
+            class="text-5xl font-bold mb-6 mt-10"
+            data-aos="fade-left"
+            data-aos-duration="800"
           >
-            <div class="relative inline-flex overflow-hidden w-full">
-              <img
-                class="transform transition-all duration-300 w-full"
-                :class="{
-                  'scale-110 filter blur-[1px]':
-                    isHovering == recentlyPlayedGames.indexOf(item),
-                }"
-                :src="`https://steamcdn-a.akamaihd.net/steam/apps/${item.appid}/header.jpg`"
-                :alt="item.name"
-              />
-              <a
-                @mouseenter="isHovering = recentlyPlayedGames.indexOf(item)"
-                @mouseleave="isHovering = null"
-                class="absolute w-full h-full flex flex-col justify-center items-center bg-black/70 text-white hover:bg-black/80 active:bg-black/85 transition-all duration-300 cursor-pointer"
-                :href="`https://store.steampowered.com/app/${item.appid}/`"
-                target="_blank"
-              >
-                <h2 class="text-2xl font-bold text-center">{{ item.name }}</h2>
-                <p class="text-lg font-semibold mt-2 -mb-2">
-                  {{
-                    recentGames.calculatePlayTime(item.playtime_2weeks, false) +
-                    " in last 2 weeks"
-                  }}
-                </p>
-                <p class="text-lg font-semibold">
-                  {{
-                    recentGames.calculatePlayTime(
-                      item.playtime_forever,
-                      false
-                    ) + " total"
-                  }}
-                </p>
-              </a>
+            Most Played Games
+          </h1>
+          <div
+            class="flex flex-wrap gap-2 justify-between"
+            v-if="mostPlayedGames != null"
+          >
+            <div
+              class="w-[calc(33.3%-0.5rem)] transition-all duration-300 border-1 border-transparent hover:border-slate-400"
+              v-for="item in mostPlayedGames"
+              :data-aos="`${animDirection(mostPlayedGames.indexOf(item))}`"
+              :data-aos-delay="`${increaseDelay()}`"
+            >
+              <div class="relative inline-flex overflow-hidden w-full">
+                <img
+                  class="transform transition-all duration-300 w-full filter"
+                  :class="{
+                    'scale-110 blur-[1px]':
+                      isHovering == mostPlayedGames.indexOf(item) + 6,
+                  }"
+                  :src="`https://steamcdn-a.akamaihd.net/steam/apps/${item.appid}/header.jpg`"
+                  :alt="item.name"
+                />
+                <a
+                  @mouseenter="isHovering = mostPlayedGames.indexOf(item) + 6"
+                  @mouseleave="isHovering = null"
+                  class="absolute w-full h-full flex flex-col justify-center items-center bg-black/70 text-white hover:bg-black/80 active:bg-black/85 transition-all duration-300 cursor-pointer"
+                  :href="`https://store.steampowered.com/app/${item.appid}/`"
+                  target="_blank"
+                >
+                  <h2 class="text-2xl font-bold text-center">
+                    {{ item.name }}
+                  </h2>
+                  <p class="text-lg font-semibold mt-2 -mb-2">
+                    {{
+                      "Total playtime: " +
+                      recentGames.calculatePlayTime(item.playtime_forever, true)
+                    }}
+                  </p>
+                  <p class="text-lg font-semibold">
+                    {{
+                      "Last played: " +
+                      topGames.calculateLastTimePlayed(item.rtime_last_played)
+                    }}
+                  </p>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section class="pb-[10%]">
-        <h1
-          class="text-5xl font-bold mb-6 mt-10"
-          data-aos="fade-left"
-          data-aos-duration="800"
-        >
-          Most Played Games
-        </h1>
-        <div
-          class="flex flex-wrap gap-2 justify-between"
-          v-if="mostPlayedGames != null"
-        >
-          <div
-            class="w-[calc(33.3%-0.5rem)] transition-all duration-300 border-1 border-transparent hover:border-slate-400"
-            v-for="item in mostPlayedGames"
-            :data-aos="`${animDirection(mostPlayedGames.indexOf(item))}`"
-            :data-aos-delay="`${increaseDelay()}`"
-          >
-            <div class="relative inline-flex overflow-hidden w-full">
-              <img
-                class="transform transition-all duration-300 w-full filter"
-                :class="{
-                  'scale-110 blur-[1px]':
-                    isHovering == mostPlayedGames.indexOf(item) + 6,
-                }"
-                :src="`https://steamcdn-a.akamaihd.net/steam/apps/${item.appid}/header.jpg`"
-                :alt="item.name"
-              />
-              <a
-                @mouseenter="isHovering = mostPlayedGames.indexOf(item) + 6"
-                @mouseleave="isHovering = null"
-                class="absolute w-full h-full flex flex-col justify-center items-center bg-black/70 text-white hover:bg-black/80 active:bg-black/85 transition-all duration-300 cursor-pointer"
-                :href="`https://store.steampowered.com/app/${item.appid}/`"
-                target="_blank"
-              >
-                <h2 class="text-2xl font-bold text-center">{{ item.name }}</h2>
-                <p class="text-lg font-semibold mt-2 -mb-2">
-                  {{
-                    "Total playtime: " +
-                    recentGames.calculatePlayTime(item.playtime_forever, true)
-                  }}
-                </p>
-                <p class="text-lg font-semibold">
-                  {{
-                    "Last played: " +
-                    topGames.calculateLastTimePlayed(item.rtime_last_played)
-                  }}
-                </p>
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </div>
+      <footer
+        class="bg-[rgb(3,3,3)] py-5 text-sm text-center text-slate-400"
+      ></footer>
     </div>
-    <footer
-      class="bg-[rgb(3,3,3)] py-5 text-sm text-center text-slate-400"
-    ></footer>
   </div>
 </template>
 <style>
@@ -315,16 +334,14 @@ const proficienciesSecondColumn = [
 
 .change-colors {
   background: linear-gradient(
-    90deg,
-    #ff3c00,
+    280deg,
     #e92c2c,
     #ff0062,
-    #cc00ff,
-    #0051ff,
     #00bbff,
-    #00ffc3
+    #00ffc3,
+    #00ff73
   );
-  animation: change-colors 10s ease infinite;
+  animation: change-colors 20s ease infinite;
   background-size: 400% 400%;
 }
 
