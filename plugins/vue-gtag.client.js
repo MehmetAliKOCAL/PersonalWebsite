3e1304c5fb1156515a83041734489e09ffea52e1
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-03-02 21:50:56 +0300

    fixed a typo that blocked the module from working

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -2,7 +2,7 @@ import VueGtag from "vue-gtag";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
-    property: {
+    config: {
       // Put your Google Analytics Measurement ID below if you want to use Google Analytics.
       // For more info: https://analytics.google.com/
 
