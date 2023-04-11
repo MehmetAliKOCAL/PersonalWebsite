a3f28ed522bca8e40f8cfa4173f45a7b5dcc784b
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-11 19:46:22 +0300

    gsap added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-windicss", "@pinia/nuxt", "@vueuse/nuxt"],
-  plugins: [{ src: "~/plugins/aos.client", mode: "client" }],
-  
+  modules: [
+    "nuxt-windicss",
+    "@pinia/nuxt",
+    "@vueuse/nuxt"
+  ],
+
+  plugins: [
+    { src: "~/plugins/aos.client", mode: "client" },
+  ],
+
+  build: {
+    transpile: ['gsap'],
+  },
+
   runtimeConfig:{
     STEAM_WEB_API_KEY: process.env.STEAM_WEB_API_KEY,
     STEAM_ID: process.env.STEAM_ID,
@@ -33,5 +44,6 @@ export default defineNuxtConfig({
       ],
       link:[{rel:'icon',type:'image/png',href:'/favicon.png'}]
     }
-  }
+  },
+
 });
