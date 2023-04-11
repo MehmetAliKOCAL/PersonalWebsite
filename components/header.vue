d6d2dd573a88b64ef31c2bab0bad692b015b6b63
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-12 00:41:16 +0300

    cleaned the overall header code and mobile header added

--- components/header.vue
+++ components/header.vue
@@ -1,69 +1,58 @@
 <script setup>
-var isHovering = ref(null);
+let mouseHoverID = ref(null);
+let isMobileMenuOpened = ref(false);
+const headerTabs = [
+  { mouseHoverID: 1, href: "/#hello", text: "Hello!" },
+  { mouseHoverID: 2, href: "/#proficiencies", text: "My Proficiencies" },
+  { mouseHoverID: 3, href: "/#currently", text: "My Works" },
+  { mouseHoverID: 4, href: "/#recentGames", text: "Games Played" },
+  { mouseHoverID: 5, href: "/#recentTracks", text: "Tracks Listened" },
+];
 </script>
 
 <template>
   <header
-    class="w-full text-slate-300 fixed top-0 z-30 text-md font-thin bg-[rgb(6,6,6)] <md:hidden"
+    class="w-full text-slate-300 fixed top-0 z-30 text-md font-thin bg-[rgb(6,6,6)]"
   >
     <div
       class="flex py-4 px-60 <2xl:px-40 <xl:px-20 <lg:px-5 items-center justify-between"
     >
-      <a href="#hello">
+      <NuxtLink to="/">
         <img src="/favicon.png" width="40" height="40" alt="GwynDev Logo"
-      /></a>
-      <nav class="flex flex-wrap gap-x-10 gap-y-4">
-        <a
-          @mouseenter="isHovering = 1"
-          @mouseleave="isHovering = null"
+      /></NuxtLink>
+
+      <nav class="<md:hidden flex flex-wrap gap-x-10 gap-y-4">
+        <NuxtLink
+          v-for="tab in headerTabs"
+          @mouseenter="mouseHoverID = tab.mouseHoverID"
+          @mouseleave="mouseHoverID = null"
           class="hover:text-sky-400 transition-colors duration-300 text-shadow-xl"
-          href="#hello"
-          >Hello!
+          :to="tab.href"
+          >{{ tab.text }}
           <hr
             class="border-b-1 border-sky-500 transition-all duration-300"
-            :class="[isHovering == 1 ? 'w-full' : 'w-0']"
-        /></a>
-        <a
-          @mouseenter="isHovering = 2"
-          @mouseleave="isHovering = null"
-          class="hover:text-sky-400 transition-colors duration-300 text-shadow-xl"
-          href="#proficiencies"
-          >Proficiencies
-          <hr
-            class="border-b-1 border-sky-500 transition-all duration-300"
-            :class="[isHovering == 2 ? 'w-full' : 'w-0']"
-        /></a>
-        <a
-          @mouseenter="isHovering = 3"
-          @mouseleave="isHovering = null"
-          class="hover:text-sky-400 transition-colors duration-300 text-shadow-xl"
-          href="#currently"
-          >Currently Working On
-          <hr
-            class="border-b-1 border-sky-500 transition-all duration-300"
-            :class="[isHovering == 3 ? 'w-full' : 'w-0']"
-        /></a>
-        <a
-          @mouseenter="isHovering = 4"
-          @mouseleave="isHovering = null"
-          class="hover:text-sky-400 transition-colors duration-300 text-shadow-xl"
-          href="#recentGames"
-          >Games
-          <hr
-            class="border-b-1 border-sky-500 transition-all duration-300"
-            :class="[isHovering == 4 ? 'w-full' : 'w-0']"
-        /></a>
-        <a
-          @mouseenter="isHovering = 5"
-          @mouseleave="isHovering = null"
-          class="hover:text-sky-400 transition-colors duration-300 text-shadow-xl"
-          href="#recentTracks"
-          >Tracks
-          <hr
-            class="border-b-1 border-sky-500 transition-all duration-300"
-            :class="[isHovering == 5 ? 'w-full' : 'w-0']"
-        /></a>
+            :class="[mouseHoverID == tab.mouseHoverID ? 'w-full' : 'w-0']"
+        /></NuxtLink>
       </nav>
+
+      <div class="md:hidden">
+        <IconsMenuIcon @click="isMobileMenuOpened = !isMobileMenuOpened" />
+        <nav
+          :class="[
+            isMobileMenuOpened ? 'visible opacity-100' : 'invisible opacity-0',
+          ]"
+          class="absolute left-0 bg-[rgb(6,6,6)] w-full h-[calc(100vh-40px)] transition-all duration-300 flex flex-col justify-center items-center"
+        >
+          <NuxtLink
+            v-for="tab in headerTabs"
+            @click="isMobileMenuOpened = !isMobileMenuOpened"
+            :to="tab.href"
+            class="text-2xl font-semibold mb-4 hover:text-sky-400 transition-colors duration-300"
+          >
+            {{ tab.text }}
+          </NuxtLink>
+        </nav>
+      </div>
     </div>
     <div
       class="w-full bg-gradient-to-r from-blue-800 via-sky-400 to-blue-800 h-[1px]"
