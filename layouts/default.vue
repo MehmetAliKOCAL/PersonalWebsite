45bd7f68063924a1c6377a725f8e34cccd1a9d8a
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-04-16 16:10:47 +0300

    code cleaning

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,43 +15,9 @@ watchEffect(() => {
   <div
     class="transition-all duration-300 min-h-screen min-w-full h-full relative bg-[rgb(10,10,10)] text-slate-200/95"
   >
+    <NuxtLoadingIndicator :color="'#0ea5e9'" />
     <Header />
     <slot />
     <Footer />
   </div>
 </template>
-<style>
-html,
-body {
-  scroll-behavior: smooth;
-  overflow-x: hidden;
-}
-
-::selection {
-  background-color: rgba(0, 255, 255, 0.8);
-  color: white;
-}
-
-::-webkit-scrollbar {
-  width: 8px;
-}
-
-::-webkit-scrollbar-track {
-  background: rgb(20, 20, 20);
-}
-
-::-webkit-scrollbar-thumb {
-  background: rgb(40, 40, 45);
-}
-
-::-webkit-scrollbar-thumb:hover {
-  background: rgb(60, 60, 68);
-}
-
-:target:before {
-  content: "";
-  display: block;
-  height: 80px;
-  margin: -80px 0 0;
-}
-</style>
---
+++ pages/projectDetails/[id].vue
@@ -0,0 +1,51 @@
+<script setup>
+const projects = [
+  {
+    title: "Volcanoids Mod",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur quas, numquam dicta deleniti sit, itaque corrupti, a harum delectus corporis ipsa ea. Iusto consectetur harum quisquam totam quibusdam. Sint, eligendi!",
+    tech: ["Volcanoids Mod SDK"],
+  },
+  {
+    title: "GwynDev",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur quas, numquam dicta deleniti sit, itaque corrupti, a harum delectus corporis ipsa ea. Iusto consectetur harum quisquam totam quibusdam. Sint, eligendi!",
+    tech: ["HTML5", "CSS3", "WindiCSS", "Vue.js", "Nuxt.js"],
+  },
+  {
+    title: "AhniWeb",
+    content:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur quas, numquam dicta deleniti sit, itaque corrupti, a harum delectus corporis ipsa ea. Iusto consectetur harum quisquam totam quibusdam. Sint, eligendi!",
+    tech: ["HTML5", "CSS3", "TailwindCSS", "Javascript", "Vue.js", "Nuxt.js"],
+  },
+];
+const project = projects[useRoute().params.id];
+</script>
+<template>
+  <div
+    class="mx-auto px-60 <2xl:px-40 <xl:px-20 <lg:px-5 py-90 flex items-center justify-center flex-col"
+  >
+    <!-- <img
+      src="https://images.wallpaperscraft.com/image/single/mountains_mountain_range_peaks_149574_1920x1080.jpg"
+      :alt="project.title"
+      class="w-full rounded-md"
+    />
+    <h1 class="text-4xl <md:text-3xl font-bold my-10 text-center">
+      {{ project.title }}
+    </h1>
+    <p>
+      Technologies Used:
+      <div v-for="tech in project.tech" :key="tech" class="inline">{{ tech }} {{ tech!=project.tech[project.tech.length-1] ? ', ' :''}}</div>
+    </p>
+    <p class="text-2xl <md:text-xl font-normal text-slate-400">
+      {{ project.content }}
+    </p> -->
+    <p>Sorry, this page is under construction.🥸</p>
+    <NuxtLink
+      to="/"
+      class="bg-black py-3 px-10 mt-10 rounded-md border-sky-400 border-1 transition-all duration-300 hoverShadow"
+    >
+      Homepage
+    </NuxtLink>
+  </div>
+</template>
