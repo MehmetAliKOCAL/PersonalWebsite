12d7c26a49625353b5e431a2b6778ee4563175f4
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-05-12 20:50:25 +0300

    added an equalizer animation

--- windi.config.ts
+++ windi.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from "windicss/helpers";
 
 export default defineConfig({
-    theme:{
-        extend:{
-            width:{
-                fit:'fit-content'
-            },
-            backgroundClip:{
-                padding:'padding-box'
-            },
-            boxShadow:{
-                'skyBloom':'0px 0px 20px 1px rgba(90, 90, 250, 0.8)'
-            }
-        }
-    }
+  theme: {
+    extend: {
+      width: {
+        fit: "fit-content",
+      },
+      backgroundClip: {
+        padding: "padding-box",
+      },
+      boxShadow: {
+        skyBloom: "0px 0px 20px 1px rgba(90, 90, 250, 0.8)",
+      },
+      animation: {
+        sound: "sound 1000ms infinite",
+      },
+    },
+  },
 });
