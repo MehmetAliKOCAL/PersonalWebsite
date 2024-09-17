aed8325e1b04acbfa611bc87fe2b6b1b8306ce32
GwynDev <root.gwyn@hotmail.com>
2024-09-17 03:03:08 +0300

    style: changed the text hover colors, border colors, text sizes

--- components/footer.vue
+++ components/footer.vue
@@ -1,23 +1,27 @@
 <script setup>
-  import { useGlobalVariablesStore } from '~/store/globalVariables.js';
-  import { storeToRefs } from 'pinia';
-  const globalVariables = useGlobalVariablesStore();
-  const { lang } = storeToRefs(globalVariables);
-  let isHovering = ref(null);
+import { useGlobalVariablesStore } from "@/store/globalVariables.js";
+import { storeToRefs } from "pinia";
+const globalVariables = useGlobalVariablesStore();
+const { lang } = storeToRefs(globalVariables);
+let isHovering = ref(null);
 
-  const eMail = 'root.gwyn@hotmail.com';
-  const socials = [
-    { name: 'LinkedIn', link: 'https://www.linkedin.com/in/gwyndev/' },
-    { name: 'GitHub', link: 'https://github.com/MehmetAliKOCAL' },
-    { name: 'Discord', link: 'https://discordapp.com/users/554061262229864458' },
-    { name: 'Reddit', link: 'https://www.reddit.com/user/Primary_Ad5726' },
-    { name: 'LastFM', link: 'https://www.last.fm/user/GwynDev' },
-    { name: 'YouTube', link: 'https://www.youtube.com/channel/UCqsbqOqrLaKWaJ-1fB1RqyQ' },
-  ];
+const eMail = "root.gwyn@hotmail.com";
+const socials = [
+  { name: "LinkedIn", link: "https://www.linkedin.com/in/gwyndev/" },
+  { name: "GitHub", link: "https://github.com/MehmetAliKOCAL" },
+  { name: "Discord", link: "https://discordapp.com/users/554061262229864458" },
+  { name: "LastFM", link: "https://www.last.fm/user/GwynDev" },
+  {
+    name: "YouTube",
+    link: "https://www.youtube.com/channel/UCqsbqOqrLaKWaJ-1fB1RqyQ",
+  },
+];
 </script>
 
 <template>
-  <div class="w-full bg-gradient-to-r from-blue-800 via-sky-400 to-blue-800 h-[1px] relative" />
+  <div
+    class="w-full bg-gradient-to-r from-blue-800 via-sky-500 to-blue-800 h-[1px] relative"
+  />
   <footer
     class="bg-[rgb(6,6,6)] py-10 font-mono relative z-10 mx-auto px-60 <2xl:px-40 <xl:px-20 <lg:px-5"
   >
@@ -26,7 +30,9 @@
     </h1>
     <div class="flex flex-col justify-center">
       <div class="flex flex-col">
-        <h2 class="mb-3 w-fit mx-auto text-lg font-semibold border-b-2 border-sky-500 px-2">
+        <h2
+          class="mb-3 w-fit mx-auto text-lg font-semibold border-b-2 border-sky-600 px-2"
+        >
           {{ lang.footerSocialsTitle }}
         </h2>
         <nav class="flex flex-row flex-wrap w-full gap-x-4 justify-center">
@@ -46,8 +52,8 @@
                 class="w-7 mr-1"
               />
               <p
-                class="text-sm transition-colors duration-300"
-                :class="{ 'text-sky-500': isHovering === social.name }"
+                class="text-sm transition-colors text-gray-400 duration-300"
+                :class="{ 'text-slate-100': isHovering === social.name }"
               >
                 {{ social.name }}
               </p>
@@ -56,12 +62,14 @@
         </nav>
       </div>
       <div class="w-fit mx-auto mt-5">
-        <h2 class="mb-3 w-fit mx-auto text-lg font-semibold border-b-2 border-sky-500 px-2">
+        <h2
+          class="mb-3 w-fit mx-auto text-lg font-semibold border-b-2 border-sky-600 px-2"
+        >
           {{ lang.footerContactTitle }}
         </h2>
         <a
           :href="`mailto:${eMail}`"
-          class="hover:text-sky-500 transition-colors duration-300 text-sm"
+          class="hover:text-slate-100 text-gray-400 transition-colors duration-300 text-sm"
         >
           {{ lang.footerMailMe }} <IconsClick class="w-6" />
         </a>
@@ -69,14 +77,14 @@
     </div>
   </footer>
   <div
-    class="bg-[rgb(6,6,6)] py-6 border-t-1 border-gray-900 flex items-center justify-center inline-block"
+    class="bg-[rgb(6,6,6)] py-3 text-sm border-t-1 border-gray-900 flex items-center justify-center inline-block"
   >
     <NuxtLink
       to="https://github.com/MehmetAliKOCAL/PersonalWebsite"
       target="_blank"
-      class="text-gray-400 hover:text-sky-500 transition-all duration-300 text-center mx-auto px-60 <2xl:px-40 <xl:px-20 <lg:px-5"
+      class="text-gray-400 hover:text-slate-100 transition-all duration-300 text-center mx-auto px-60 <2xl:px-40 <xl:px-20 <lg:px-5"
     >
-      {{ lang.footerOpenSource }}</NuxtLink
-    >
+      {{ lang.footerOpenSource }}
+    </NuxtLink>
   </div>
 </template>
